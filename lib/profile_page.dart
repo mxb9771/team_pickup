c24d8470c09d39bbfd114b3e3c26b8666fe424b1
Lucas <Lucasgolden14@gmail.com>
2019-10-27 19:10:32 -0400

    Profile Page

--- lib/profile_page.dart
+++ lib/profile_page.dart
@@ -5,6 +5,40 @@ class ProfilePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return null;
+    return 
+    Material(
+      child:  new Scaffold(
+        appBar: AppBar(
+          title: Text('Profile',
+          textAlign: TextAlign.center,),
+        ),
+        body: new Stack(
+          children: <Widget>[
+        new Container(color: Colors.white,
+        ),
+        new Container(
+          alignment: Alignment(0, -0.6),
+          child: Container(
+            child: Image.asset('assets/images/profile.jpg'),
+            constraints: BoxConstraints(maxHeight: 300, maxWidth: 300),
+          ),
+        ),
+        new Container(
+          alignment: Alignment(0, 0.3),
+          child: Text('Lucas Golden',
+          style: TextStyle(fontSize: 50),),
+        ),
+        new Container(
+          alignment: Alignment(0, 0.5),
+          child: Container(
+            child: Text("5'7 Point Guard with a lot of fight in him",
+            style: TextStyle(fontSize: 30),),
+            constraints: BoxConstraints(maxHeight: 300, maxWidth: 300),
+          )
+        )
+      ]
+    )
+    )
+    );
   }
 }
