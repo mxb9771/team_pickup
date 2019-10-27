011524e5e36c18d23a1ceae3771a039dd06403d1
Matthew <mxb9771@rit.edu>
2019-10-27 14:35:27 -0400

    added navigation buttons to main page

---
+++ lib/profile_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+//home page to have routes to all other pages
+class ProfilePage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return null;
+  }
+}
---
+++ lib/teams_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+//home page to have routes to all other pages
+class TeamsPage extends StatelessWidget {
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return null;
+  }
+}
